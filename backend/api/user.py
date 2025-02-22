b523c36eb9417c5418864da070ada58c45a6a8c4
happytech <happytech.web@gmail.com>
2025-02-22 16:50:21 +0800

    modified create structure to fit in

--- backend/api/user.py
+++ backend/api/user.py
@@ -13,7 +13,8 @@ async def create_new_user(user: UserCreate):
     :return: user id
     """
     user_id = create_user(user.username, user.email, user.password, user.profile_picture)
-    return get_user_by_id(user_id)
+    cur_user = get_user_by_id(user_id)
+    return objectID2str(cur_user)
 
 # 用户认证路由
 @router.post("/authenticate/", response_model=AuthResponse)
@@ -43,6 +44,9 @@ async def get_user(user_id: str):
         raise HTTPException(status_code=404, detail="User not found")
     
     # 将返回的 MongoDB 对象转换为 Pydantic 需要的格式
+    return objectID2str(user)
+
+def objectID2str(user):
     user_response = UserResponse(
         id=str(user["_id"]),  # 将 _id 转换为字符串
         username=user["username"],
@@ -52,6 +56,7 @@ async def get_user(user_id: str):
         collaborations=[str(collab) for collab in user["collaborations"]]
     )
     return user_response
+    
 # # 获取用户信息路由
 # @router.get("/{user_id}", response_model=UserResponse)
 # async def get_user(user_id: str):
