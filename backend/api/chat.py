d867d5129a22dd8aba7353911ed5af6b1b84215d
xqy <2795143323@qq.com>
2025-02-22 17:13:39 +0800

    test

--- backend/api/chat.py
+++ backend/api/chat.py
@@ -25,7 +25,7 @@ async def chat(message: str):
     """
     response_text = await ai_service.chat(message)
     return Message(sayer="assistant", text=response_text)
-
+''''''
 @router.post("/with-file", response_model=Message)
 async def chat_with_file(message: str, file: UploadFile = File(...)):
     """
@@ -33,22 +33,24 @@ async def chat_with_file(message: str, file: UploadFile = File(...)):
     """
     # 保存上传的文件到指定文件夹
     if platform.system() == "Windows":
-        upload_folder = "backend\\services\\DataStructuring\\DataStructuring\\SourceData"
+        upload_folder = "..\services\\DataStructuring\\DataStructuring\\SourceData"
     else:
-        upload_folder = "backend/services/DataStructuring/DataStructuring/SourceData"
+        upload_folder = "../services/DataStructuring/DataStructuring/SourceData"
     
     file_path = os.path.join(upload_folder, file.filename)
-    
+    # 删除upload_folder文件夹里所有文件
+    for f in glob.glob(os.path.join(upload_folder, "*")):
+        os.remove(f)
     with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        await shutil.copyfileobj(file.file, buffer)
     
     # 这里需要添加文件处理逻辑
     await main_process()
     await target_to_json()
     if platform.system() == "Windows":
-        json_folder = "backend\\services\\DataStructuring\\DataStructuring\\JsonData"
+        json_folder = "..\\services\\DataStructuring\\DataStructuring\\JsonData"
     else:
-        json_folder = "backend/services/DataStructuring/DataStructuring/JsonData"
+        json_folder = "../services/DataStructuring/DataStructuring/JsonData"
     
    
     # 获取json_folder文件夹里所有文件的内容
